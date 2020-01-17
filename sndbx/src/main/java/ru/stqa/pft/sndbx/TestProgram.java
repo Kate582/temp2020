c7fea75b6c1a04646212d02121dcd1458e861810
Kate582 <40356227+Kate582@users.noreply.github.com>
2020-01-17 16:18:02 +0500

    Добавлено несколько функций

--- sndbx/src/main/java/ru/stqa/pft/sndbx/TestProgram.java
+++ sndbx/src/main/java/ru/stqa/pft/sndbx/TestProgram.java
@@ -3,7 +3,27 @@ package ru.stqa.pft.sndbx;
 public class TestProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    hello("world");
+    hello("user");
+
+    double l = 5;
+    System.out.println("Площадь для квадрата " + l + " = " + area(l));
+
+    double a = 2;
+    double b = 3;
+    System.out.println("Площадь = " + area(a, b));
+  }
+
+  public static void hello(String str) {
+    System.out.println("Hello, " + str + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
